55bec5044483b085b61d70dc0f6c13440d9efa3c
Gustavo Campagnin <gcampagnin@gmail.com>
2025-10-17 08:55:28 -0300

    feat: add auth-links schema

---
+++ drizzle/0004_closed_xorn.sql
@@ -0,0 +1,8 @@
+CREATE TABLE "auth_links" (
+	"id" text PRIMARY KEY NOT NULL,
+	"code" text NOT NULL,
+	"user_id" text NOT NULL,
+	"created_at" timestamp DEFAULT now()
+);
+--> statement-breakpoint
+ALTER TABLE "auth_links" ADD CONSTRAINT "auth_links_user_id_users_id_fk" FOREIGN KEY ("user_id") REFERENCES "public"."users"("id") ON DELETE no action ON UPDATE no action;
