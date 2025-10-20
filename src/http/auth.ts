7312efb8d515d4ac57f53d75670bfb85c85e6f63
Gustavo Campagnin <gcampagnin@gmail.com>
2025-10-20 17:24:13 -0300

    feat: add route for logged-in user profile

--- src/http/auth.ts
+++ src/http/auth.ts
@@ -1,6 +1,7 @@
 import { jwt } from '@elysiajs/jwt'
 import Elysia, { t, type Static } from 'elysia'
 import { env } from '../env'
+import { cookie } from '@elysiajs/cookie'
 
 const jwtPayload = t.Object({
   sub: t.String(),
@@ -8,29 +9,47 @@ const jwtPayload = t.Object({
 })
 
 export const auth = new Elysia()
+  .use(cookie())
   .use(
     jwt({
       secret: env.JWT_SECRET_KEY,
       schema: jwtPayload,
     }),
   )
-  .derive({ as: 'scoped' }, ({ jwt, cookie: { auth } }) => {
+  .derive({ as: 'scoped' }, ({ jwt, cookie }) => {
     return {
       signUser: async (payload: Static<typeof jwtPayload>) => {
         const token = await jwt.sign(payload)
 
-        if (!auth) {
+        if (!cookie.auth) {
           throw new Error('Cookie is not found.')
         }
 
-        auth.value = token
-        auth.httpOnly = true
-        auth.maxAge = 60 * 60 * 24 * 7 // 7 days
-        auth.path = '/'
+        cookie.auth.value = token
+        cookie.auth.httpOnly = true
+        cookie.auth.maxAge = 60 * 60 * 24 * 7 // 7 days
+        cookie.auth.path = '/'
       },
 
       signOut: async () => {
-        auth?.remove()
+        cookie.auth?.remove()
+      },
+
+      getCurrentUser: async () => {
+        const token =
+          typeof cookie?.auth?.value === 'string'
+            ? cookie.auth.value
+            : undefined
+        const payload = await jwt.verify(token)
+
+        if (!payload) {
+          throw new Error('Unauthorized.')
+        }
+
+        return {
+          userId: payload.sub,
+          restauranteId: payload.restaurantId,
+        }
       },
     }
   })
