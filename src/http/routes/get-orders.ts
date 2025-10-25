1e0780e60b5bcc64045ef20ddcf1a5b0fd85c2aa
Gustavo Campagnin <gcampagnin@gmail.com>
2025-10-25 19:56:55 -0300

    feat: add order sorting

--- src/http/routes/get-orders.ts
+++ src/http/routes/get-orders.ts
@@ -4,7 +4,7 @@ import { db } from '../../db/connection'
 import { UnauthorizedError } from '../errors/unauthorized-error'
 import { createSelectSchema } from 'drizzle-typebox'
 import { orders, users } from '../../db/schema'
-import { and, count, eq, getTableColumns, ilike } from 'drizzle-orm'
+import { and, count, desc, eq, ilike, sql } from 'drizzle-orm'
 
 export const getOrders = new Elysia().use(auth).get(
   '/orders',
@@ -16,17 +16,21 @@ export const getOrders = new Elysia().use(auth).get(
       throw new UnauthorizedError()
     }
 
-    const orderTableColumns = getTableColumns(orders)
-
     const baseQuery = db
-      .select(orderTableColumns)
+      .select({
+        orderId: orders.id,
+        createdAt: orders.createdAt,
+        status: orders.status,
+        total: orders.totalInCents,
+        customerName: users.name,
+      })
       .from(orders)
       .innerJoin(users, eq(users.id, orders.customerId))
       .where(
         and(
           eq(orders.restaurantId, restauranteId),
           orderId ? ilike(orders.id, `%${orderId}%`) : undefined,
-          status ? eq(orders.status, status) : undefined,
+          status ? eq(orders.id, status) : undefined,
           customerName ? ilike(users.name, `%${customerName}%`) : undefined,
         ),
       )
@@ -37,7 +41,19 @@ export const getOrders = new Elysia().use(auth).get(
         .select()
         .from(baseQuery.as('baseQuery'))
         .offset(pageIndex * 10)
-        .limit(10),
+        .limit(10)
+        .orderBy((fields) => {
+          return [
+            sql`CASE ${fields.status}
+              WHEN 'pending' THEN 1
+              WHEN 'processing' THEN 2
+              WHEN 'delivering' THEN 3
+              WHEN 'delivered' THEN 4
+              WHEN 'canceled' THEN 99
+            END`,
+            desc(fields.createdAt),
+          ]
+        }),
     ])
 
     const amountOfOrders = amountOfOrdersQuery[0]?.count
